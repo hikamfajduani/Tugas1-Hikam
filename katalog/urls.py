0d445a60b017d8c55cf337ef308fc711a9788822
hikam.fajduani <hikam.fajduani@ui.ac.id>
2022-09-14 08:31:59 +0700

    Menambahkan file baru

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import show_katalog, show_wishlist
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', show_katalog, name='show_katalog'),
+]
